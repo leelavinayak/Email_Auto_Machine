b3f19e5c7ab970e817dc236c1a89d8704c36d235
LeelaVinayak Kothakota <leelavinayakkothakota155@gmail.com>
2026-08-22 07:32:57 +0530

    Normalize Vercel API function paths

--- api/[...path].js
+++ api/[...path].js
@@ -2,5 +2,10 @@ import app, { initializeDB } from '../server/index.js';
 
 export default async function handler(req, res) {
   await initializeDB();
+
+  if (!req.url.startsWith('/api')) {
+    req.url = `/api${req.url.startsWith('/') ? '' : '/'}${req.url}`;
+  }
+
   return app(req, res);
 }
